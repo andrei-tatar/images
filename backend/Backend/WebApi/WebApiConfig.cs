0d1cdf538332f726afd8d1c0847ffaf269acc154
Andrei Tatar <tatarandrei@gmail.com>
2017-10-26 20:45:15 +0300

    use exception handler to return validation errors

--- backend/Backend/WebApi/WebApiConfig.cs
+++ backend/Backend/WebApi/WebApiConfig.cs
@@ -1,4 +1,15 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using Common;
+using FluentValidation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Unity;
 
 namespace Backend.WebApi
@@ -7,6 +18,9 @@ namespace Backend.WebApi
     {
         public WebApiConfig(IUnityContainer container)
         {
+            Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
             DependencyResolver = new UnityHttpDependencyResolver(container, DependencyResolver);
 
             Routes.MapHttpRoute(
@@ -14,6 +28,42 @@ namespace Backend.WebApi
                 routeTemplate: "{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            Filters.Add(new WebApiExceptionFilter(Formatters.JsonFormatter.SerializerSettings));
+        }
+    }
+
+    public class WebApiExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly JsonSerializerSettings _jsonSetings;
+
+        public WebApiExceptionFilter(JsonSerializerSettings jsonSetings)
+        {
+            _jsonSetings = jsonSetings;
+        }
+
+        public override async Task OnExceptionAsync(HttpActionExecutedContext context, CancellationToken cancellationToken)
+        {
+            var validationException = context.Exception as ValidationException;
+            if (validationException != null)
+            {
+                context.Response = new HttpResponseMessage
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(new
+                    {
+                        ValidationErrors = validationException.Errors.Select(s => new
+                        {
+                            Field = s.PropertyName.ToCamelCase(),
+                            Code = s.ErrorCode,
+                        }).ToArray()
+                    }, _jsonSetings), Encoding.UTF8, "application/json"),
+                    StatusCode = HttpStatusCode.BadRequest,
+
+                };
+                return;
+            }
+
+            await base.OnExceptionAsync(context, cancellationToken);
         }
     }
 }
---
+++ backend/Common/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public static class ExtensionMethods
+    {
+        public static string ToCamelCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return char.ToLower(input[0]) + input.Substring(1);
+        }
+    }
+}
