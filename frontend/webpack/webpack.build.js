edd7233dc8df08c16f1eda7b97f478f5eee1a397
Andrei Tatar <tatarandrei@gmail.com>
2017-10-30 00:22:46 +0200

    add webpack build config

--- frontend/webpack/webpack.build.js
+++ frontend/webpack/webpack.build.js
@@ -1,9 +1,7 @@
 var loaders = require('./loaders');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var path = require('path');
-var historyApiFallback = require('connect-history-api-fallback')
 
 module.exports = {
     entry: ['./src/app.ts'],
@@ -11,32 +9,28 @@ module.exports = {
         filename: 'build.js',
         path: path.resolve(__dirname, '..', 'dist'),
     },
+    devtool: 'source-map',
     resolve: {
         extensions: ['.ts', '.js', '.json']
     },
     resolveLoader: {
         modules: ['node_modules']
     },
-    devtool: 'inline-eval-cheap-source-map',
     plugins: [
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                warning: false,
+                mangle: true,
+                comments: false
+            }
+        ),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: 'body',
             hash: true
         }),
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 6002,
-            server: {
-                baseDir: './dist'
-            },
-            ui: false,
-            online: false,
-            notify: false,
-            middleware: [ historyApiFallback()],
-        }),
     ],
     module: {
         loaders: loaders
-    }
+    },
 };
